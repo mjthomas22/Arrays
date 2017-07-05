19e5a5cfb1e63f500d44fa9a49631e90f02440f1
mjthomas22 <mthomas22@gmail.com>
2017-07-05 13:44:26 -0400

    Fourth Commit

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -62,8 +62,35 @@ namespace Arrays
             //int[] ageOfStudents = new int[21];
             //ageOfStudents[0] = int.Parse (Console.ReadLine());
 
+            //string[] restuarantsInMall = { "Subway", "Dunkin Donuts", "Saki", "Red Lobster", "Chick Fil-A" };
+            //Console.WriteLine(restuarantsInMall.Length);
 
-        
+            ////For Review
+            //int[] intVariable;
+            // //Declare array when i know the elements
+            //char[] letterInFirstName = { 'D', 'a', 'n', 'i', 'e', 'l' };
+            ////Decalre and intiaize when I do not know the elements
+            //string[] unKnown = new string[5];
+
+            ////To Practice using .Length, create a char array of letter in your middle name
+            //// and print # of chars to the console.
+
+            //char[] middleName = { 'J', 'o', 's', 'e', 'p', 'h' };
+            //Console.WriteLine(middleName.Length);
+
+            ////decalre and intialize a string array.
+            ////using a length property, print the second to last element in the array
+
+            //string[] food = { "pizza", "wings", "burgers", "hotdogs" };
+            //int number = food.Length;
+            //Console.WriteLine(food[number - 2]);
+
+            //Index of is a method used to search an array for a specified value and returns the index postion of the first matching value found
+
+            int[] bestYearsEver = { 1985, 1999, 1991, 2000, 1985 };
+            Console.WriteLine(Array.IndexOf(bestYearsEver, 1985));
+
+
         }
     }
 }
